3c2694795b1eb44ac4edbde22cd8fd3d662be24e
Abhinav kumar choubey <abhinavchoubey22@gmail.com>
2026-01-12 16:55:11 +0530

    basic setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+
+export default function App() {
+  return (
+    <div className="bg-red-500 h-screen flex items-center justify-center text-white text-3xl">
+  Color test
+</div>
+
+  )
+}
+
