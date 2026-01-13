f38cca12a871661c92f2b5e4b65622fb398ed0fd
Abhinav kumar choubey <abhinavchoubey22@gmail.com>
2026-01-13 18:15:52 +0530

    about

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import Hero from "./components/Hero.jsx";
-
+import About from "./components/about.jsx";
 export default function App() {
   return (
     <>
       <Hero></Hero>
+      <About></About>
     </>
 
   )
---
+++ src/components/About.jsx
@@ -0,0 +1,47 @@
+import "./about.css";
+import phone from "../assets/images/About/android-smartphone-free-mockup 1.png";
+import { FaCheck } from "react-icons/fa";
+
+export default function About() {
+  return (
+    <section className="about">
+      <h2 className="about-title">About Our App</h2>
+      <p className="about-desc">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra nunc ante velit vitae.
+        Est tellus vitae, nullam lobortis enim. Faucibus amet etiam tincidunt rhoncus.
+      </p>
+
+      <div className="about-content">
+        <div className="about-image">
+          <img src={phone} alt="App mockup" />
+        </div>
+
+        <div className="about-cards">
+          <div className="about-card">
+            <FaCheck className="icon" />
+            <div>
+              <h4>Creative Design</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+          </div>
+
+          <div className="about-card">
+            <FaCheck className="icon" />
+            <div>
+              <h4>Easy To Use</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+          </div>
+
+          <div className="about-card">
+            <FaCheck className="icon" />
+            <div>
+              <h4>Best User Experience</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
